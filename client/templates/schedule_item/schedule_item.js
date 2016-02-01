0415c2ece3a47982e99a8ecee2763bd2837b934b
Rathius <rathius@icloud.com>
2016-02-01 12:18:32 -0500

    FINALLY got the schedule_item.js file worked out and not giving errors but the map still doesn't show up yet.

--- client/templates/schedule_item/schedule_item.js
+++ client/templates/schedule_item/schedule_item.js
@@ -20,32 +20,29 @@ if (Meteor.isClient) {
 					current: Geolocation.latLng(),
 					start: Geolocation.position(),
 					dest: (Schedule.findOne({_id: this._id}).scheduleLat, Schedule.findOne({_id: this._id}).scheduleLng),
-					
-					map = new google.maps.Map('map'),({
-						// get currentLocation and set map options
-						center: current,
-						zoom: mapZoom
-					});
+					center: current,
+					zoom: mapZoom
 				}
 			}
-		}
+		},
+		map: new google.maps.Map(('map'), mapOptions)
 	});
-	
+
 	Template.schedule_item.onCreated(function() {
 		// We can use the `ready` callback to interact with the map API once the map is ready.
 	    GoogleMaps.ready('map', function(map) {
 			// Add a marker to the map once it's ready
-			var directionsDisplay = new google.maps.DirectionsRenderer({
+			var directionsDisplay = new google.maps.DirectionsRenderer(
 					map.instance
-				});
-				
+				);
+
 			// Set destination, origin and travel mode.
 			var request = {
 				destination: dest,
 				origin: start,
 				travelMode: google.maps.TravelMode.DRIVING
 			};
-			
+
 			// Pass the directions request to the directions service.
 			var directionsService = new google.maps.DirectionsService();
 			directionsService.route(request, function(response, status) {
@@ -56,15 +53,5 @@ if (Meteor.isClient) {
 			});
 	    });
 	});
-	
-	
-	/*Template.schedule_item.events({
-		'click .get-directions': function(){
-			geolocationError: function() {
-				var error = Geolocation.error();
-				return error && error.message;
-			},
-			
-		}
-	});*/
+
 }
